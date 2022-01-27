c3f677942dc5ee126fe21b9eff79d068293709a7
GodOfWarOP <51237086+godofwarOP@users.noreply.github.com>
2022-01-27 17:09:35 +0530

    added more info in about page and css

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,71 @@
 import { Link } from "react-router-dom";
 import Card from "../components/shared/Card";
+import { FaChalkboardTeacher, FaHome } from "react-icons/fa";
+import { SiGithub, SiUdemy } from "react-icons/si";
 
 function AboutPage() {
-  return <Card>
+  return (
+    <Card>
       <div className="about">
-          <h1>About This Project</h1>
-          <p>This is a React app to leave feedback for a product or service</p>
-          <p>Version: 1.0.0</p>
+        <h1>Feedback App</h1>
+        <p>
+          This is a React app to leave feedback for a product or service <br />{" "}
+          This project is part of the React Front To Back Udemy course by{" "}
+          <strong>Brad Traversy</strong>.
+        </p>
+        <div className="links">
+          <h1>Links</h1>
+          <div className="link">
+            <a
+              className="link"
+              href="https://www.udemy.com/course/modern-react-front-to-back/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiUdemy />
+              <span style={{ marginLeft: ".2rem" }}>Course Link</span>
+            </a>
+          </div>
+          <div className="link">
+            <a
+              className="link"
+              href="https://github.com/godofwarOP/feedback-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiGithub />
+              <span style={{ marginLeft: ".2rem" }}>Repository Link</span>
+            </a>
+          </div>
+          <div className="link">
+            <a
+              className="link"
+              href="https://traversymedia.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaChalkboardTeacher />
+              <span style={{ marginLeft: ".2rem" }}>Brad Traversy</span>
+            </a>
+          </div>
+        </div>
 
-          <p>
-              <Link to={{
-                  pathname:"/"
-              }}>Back to home</Link>
-          </p>
+        <hr />
+
+        <p>
+          <Link
+            to={{
+              pathname: "/",
+            }}
+            className="link"
+          >
+            <FaHome />
+            <span style={{ marginLeft: ".2rem" }}>Back To Home</span>
+          </Link>
+        </p>
       </div>
-  </Card>;
+    </Card>
+  );
 }
 
 export default AboutPage;
